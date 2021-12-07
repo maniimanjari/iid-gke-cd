358735c4ff7d8a9e5e8f8f3859e7ee5ce72a32f6
Gidda, Ramesh <RGidda@west.com>
2021-12-07 15:28:43 +0530

    test

---
+++ prepare-build.sh
@@ -0,0 +1,11 @@
+mkdir -p out
+for i in $(ls -d */); do
+  if [[  $i == ci* ]]; then
+    echo $i
+      for file in $PWD/${i}/*; do
+        echo "$(basename $PWD/${i})-$(basename "$file")"
+        cp $file ./out/$(basename $PWD/${i})-$(basename "$file");
+      done;
+  fi
+done
+echo $(ls $PWD/out)
