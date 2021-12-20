114231c25514bb8fd50c11bf03ffd3d3a1372260
Gidda, Ramesh <RGidda@west.com>
2021-12-20 15:09:08 +0530

    iid build

--- prepare-build.sh
+++ prepare-build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 mkdir -p stage-apis
 mkdir -p stage-portals
-mkdir -p push-name-space
-apt-get install envsubst
+#mkdir -p push-name-space
+#apt-get install envsubst
 echo "current directory *** $(ls $PWD)"
-export $(cat ./qa.env | egrep -v "(^#.*|^$)" | xargs)
+#export $(cat ./qa.env | egrep -v "(^#.*|^$)" | xargs)
+echo "$_APPLICTION_PROFILE  rams >>> $_APPLICTION_PROFILE"
+echo "CLOUDSDK_CORE_PROJECT >>> rams $CLOUDSDK_CORE_PROJECT"
+echo "portals CLOUDSDK_CONTAINER_CLUSTER >>> $CLOUDSDK_CONTAINER_CLUSTER"
 for i in $(ls -d */); do
   if [[  $i == ci-* || $i == file* || $i == ciapi-* ]]; then
     echo $i
@@ -20,14 +23,14 @@ for i in $(ls -d */); do
         cp $file  ./stage-portals/$(basename $PWD/${i})-$(basename "$file");
       done;
   fi
-  if [[  $i == name-spa* ]]; then
-    echo $i
-      for file in $PWD/${i}/*; do
-        echo "push-name-space >>> $(basename $PWD/${i})-$(basename "$file")"
-        cp $file  ./push-name-space/$(basename $PWD/${i})-$(basename "$file");
-      done;
-  fi
+#  if [[  $i == name-spa* ]]; then
+#    echo $i
+#      for file in $PWD/${i}/*; do
+#        echo "push-name-space >>> $(basename $PWD/${i})-$(basename "$file")"
+#        cp $file  ./push-name-space/$(basename $PWD/${i})-$(basename "$file");
+#      done;
+#  fi
 done
 echo "Execution directory for apis deployment *** $(ls $PWD/stage-apis)"
 echo "Execution directory for portals deployment *** $(ls $PWD/stage-portals)"
-echo "Execution directory for name space deployment *** $(ls $PWD/push-name-space)"
+#echo "Execution directory for name space deployment *** $(ls $PWD/push-name-space)"
