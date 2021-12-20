4894cbd3393b308aa9b24fb166ac48af773b7e75
Gidda, Ramesh <RGidda@west.com>
2021-12-20 15:37:35 +0530

    iid build

--- prepare-build.sh
+++ prepare-build.sh
@@ -32,5 +32,7 @@ for i in $(ls -d */); do
 #  fi
 done
 echo "Execution directory for apis deployment *** $(ls $PWD/stage-apis)"
+find $PWD/stage-apis/ -type f -exec sed -i "s/%_APP_PROFILE%/${_APPLICTION_PROFILE}/g" {} +
+echo "UUID >>>  rams >>> $(cat /proc/sys/kernel/random/uuid)"
 echo "Execution directory for portals deployment *** $(ls $PWD/stage-portals)"
 #echo "Execution directory for name space deployment *** $(ls $PWD/push-name-space)"
