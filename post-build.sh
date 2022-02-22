9784b39e690097ff7c8d508d42ac0faafcdfabee
Mani Konjara <mani.konjara@west.com>
2022-02-22 20:43:50 +0530

    adding files

---
+++ post-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for i in $(ls -d */ | cut -f1 -d'/'); do
+  if [[  $i == ci-* || $i == ciapi-* || $i == cif-* || $i == westid-* || $i == merope-* || $i == dps-* ]]; then
+    echo $i
+    src_image=$(kubectl get pods --all-namespaces -o jsonpath="{.items[*].spec.containers[*].image}" -l app=$i)
+    dest_image=$(kubectl get pods --all-namespaces -o jsonpath="{.items[*].spec.containers[*].image}" -l app=$i| sed 's/$PROJECT_ID/$_DESTINATION_PROJECT_ID/g')
+    docker pull $src_image
+    docker tag $src_image $dest_image
+    docker push $dest_image
+  fi
+done
